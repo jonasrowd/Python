d9e6155e3577e03ceb937626758a3a80ed782e7c
Jonas Machado <nhjmachado@gmail.com>
2023-10-06 21:26:02 -0300

    Evolutiva

--- open_sigs_compare_jira.py
+++ open_sigs_compare_jira.py
@@ -1,66 +1,155 @@
 import os
 import pandas as pd
 import tkinter as tk
-from tkinter import messagebox, filedialog
+from tkinter import ttk, filedialog, Label, Button, Text, Scrollbar
 
-root = tk.Tk()
-root.withdraw()  
+class IntroScreen:
+    def __init__(self, master):
+        self.master = master
+        self.master.title("Informações do Processador de Arquivos")
+        self.master.geometry("700x500")
 
-messagebox.showinfo("Instruções", "Por favor, selecione os arquivos de entrada e escolha onde o arquivo de saída será gerado.")
+        info_text = """
 
-csv_path = filedialog.askopenfilename(title="Selecione o arquivo CSV", filetypes=[("CSV files", "*.csv")])
-xlsx_path = filedialog.askopenfilename(title="Selecione o arquivo Excel", filetypes=[("Excel files", "*.xlsx")])
+O Arquivo do Sigs deve ser separado por ; e do Jira exportado para Better Excel.
 
-def process_files(csv_path, xlsx_path):
+Carrega os Arquivos:
 
-    print("Iniciando o processo...")
+- O arquivo export do Sigs é lido.
+- O arquivo arquivo em aberto da Bare e Holding é lido.
 
-    print(f"Lendo o arquivo CSV em {csv_path}...")
-    df_csv = pd.read_csv(csv_path, delimiter=';')
+Filtragem dos Dados do export do Sigs:
 
-    # Filtrando os dados com a palavra 'CAPGEMINI' na coluna 'grupo'
-    df_csv = df_csv[df_csv['Grupo'].str.contains('CAPGEMINI', case=False, na=False)]
-    print(f"Registros no CSV com 'CAPGEMINI' no grupo: {len(df_csv)}")
+- Filtra as linhas onde a coluna 'Grupo' contém a palavra 'CAPGEMINI'.
+- Limpa a coluna 'ID', removendo os espaços e excluindo linhas com valores NaN.
+- Converte a coluna 'ID' do export do Sigs para o tipo str.
 
-    print("Limpando dados do CSV...")
-    df_csv['ID'] = df_csv['ID'].str.strip()
-    df_csv = df_csv.dropna(subset=['ID'])
-    print(f"Registros no CSV após limpeza: {len(df_csv)}")
+Limpando os Dados do arquivo em aberto da Bare e Holding:
 
-    print(f"Lendo o arquivo Excel em {xlsx_path}...")
-    df_excel = pd.read_excel(xlsx_path)
-    print(f"Registros no Excel: {len(df_excel)}")
+- Limpa a coluna 'ID SIGS', removendo os espaços e excluindo linhas com valores NaN.
+- Converte a coluna 'ID SIGS' para o tipo str.
 
-    print("Limpando dados do Excel...")
-    df_excel['ID SIGS'] = df_excel['ID SIGS'].str.strip()
-    df_excel = df_excel.dropna(subset=['ID SIGS'])
-    print(f"Registros no Excel após limpeza: {len(df_excel)}")
+Comparações:
 
-    print("Padronizando tipos de dados das colunas para comparação...")
-    df_csv['ID'] = df_csv['ID'].astype(str)
-    df_excel['ID SIGS'] = df_excel['ID SIGS'].astype(str)
+- IDs do export do Sigs que não estão no arquivo em aberto da Bare e Holding são armazenados na worksheet "Cadastrar Jira".
+- IDs do arquivo em aberto da Bare e Holding que não estão no export do Sigs são armazenados na worksheet "Fechar Sigs".
 
-    print("Comparando dados entre Excel e CSV...")
-    ids_nao_encontrados = df_csv['ID'][~df_csv['ID'].isin(df_excel['ID SIGS'])]
-    print(f"IDs não encontrados no Excel: {len(ids_nao_encontrados)}")
+Exclusão de Arquivos:
 
-    # Especificando o caminho para salvar os dados não encontrados
-    output_path = filedialog.asksaveasfilename(defaultextension=".csv", filetypes=[("CSV files", "*.csv")])
-    
-    print(f"Salvando dados não encontrados em {output_path}...")
-    # Salvando todos os dados (todas as colunas) dos IDs não encontrados em um arquivo CSV
-    df_csv[~df_csv['ID'].isin(df_excel['ID SIGS'])].to_csv(output_path, index=False, sep=';', encoding='utf-8')
+- Após o processamento, é possível excluir os arquivos export do Sigs e arquivo em aberto da Bare e Holding originais.
+"""
 
-process_files(csv_path, xlsx_path)
+        scroll = Scrollbar(self.master)
+        scroll.pack(side=tk.RIGHT, fill=tk.Y)
 
-delete_files = messagebox.askyesno("Excluir Arquivos", "Deseja excluir os arquivos Excel e CSV após o processamento?")
+        txt_info = Text(self.master, wrap=tk.WORD, yscrollcommand=scroll.set)
+        txt_info.insert(tk.END, info_text)
+        txt_info.pack(padx=10, pady=10, expand=True, fill=tk.BOTH)
 
-if delete_files:
-    for file_path in [csv_path, xlsx_path]:
-        if os.path.exists(file_path):
-            os.remove(file_path)
-            print(f"Arquivo {file_path} removido com sucesso.")
-        else:
-            print(f"Arquivo {file_path} não encontrado.")
+        scroll.config(command=txt_info.yview)
 
-print("Processo concluído!")
+        btn_start = Button(self.master, text="Iniciar", command=self.start_app)
+        btn_start.pack(pady=20)
+
+    def start_app(self):
+        self.master.destroy()
+        root = tk.Tk()
+        app = App(root)
+        root.mainloop()
+class App:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Comparador Sigs x Jira")
+        self.root.geometry("400x200")
+
+        self.csv_path = None
+        self.xlsx_path = None
+
+        lbl_title = Label(self.root, text="Bem-vindo ao Comparador Sigs x Jira", font=("Arial Bold", 14))
+        lbl_title.pack(pady=10)
+
+        btn_start = Button(self.root, text="Processar", command=self.cadastrar_jira)
+        btn_start.pack(pady=10)
+
+        btn_delete = Button(self.root, text="Excluir Arquivos Utilizados", command=self.on_delete_files)
+        btn_delete.pack(pady=10)
+
+        self.lbl_status = Label(self.root, text="")
+        self.lbl_status.pack(pady=10)
+
+    def get_file_path(self, title, file_type, extension):
+        return filedialog.askopenfilename(title=title, filetypes=[(file_type, extension)])
+
+    def read_csv(self, file_path):
+        return pd.read_csv(file_path, delimiter=';')
+
+    def read_excel(self, file_path):
+        return pd.read_excel(file_path)
+
+    def clean_dataframe(self, df, column_name):
+        df[column_name] = df[column_name].str.strip()
+        df = df.dropna(subset=[column_name])
+        return df
+
+    def save_dataframe(self, df, column_name, compare_column, output_path):
+        df[~df[column_name].isin(compare_column)].to_excel(output_path, index=False, sheet_name="Cadastrar Jira")
+
+    def delete_files(self, file_paths):
+        for file_path in file_paths:
+            if os.path.exists(file_path):
+                os.remove(file_path)
+                print(f"Arquivo {file_path} removido com sucesso.")
+            else:
+                print(f"Arquivo {file_path} não encontrado.")
+
+    def cadastrar_jira(self):
+        self.csv_path = self.get_file_path("Selecione o arquivo export do Sigs", "CSV files", "*.csv")
+        if not self.csv_path:
+            self.lbl_status.config(text="Erro: Arquivo de saída não selecionado.")
+            return
+        
+        self.xlsx_path = self.get_file_path("Selecione o arquivo em aberto da Bare e Holding (Better Excel)", "Excel files", "*.xlsx")
+        if not self.xlsx_path:
+            self.lbl_status.config(text="Erro: Arquivo de saída não selecionado.")
+            return
+        
+        try:
+            df_csv = self.read_csv(self.csv_path)
+            df_csv = df_csv[df_csv['Grupo'].str.contains('CAPGEMINI', case=False, na=False)]
+            df_csv = self.clean_dataframe(df_csv, 'ID')
+            df_csv['ID'] = df_csv['ID'].astype(str)
+
+            df_excel = self.read_excel(self.xlsx_path)
+            df_excel = self.clean_dataframe(df_excel, 'ID SIGS')
+            df_excel['ID SIGS'] = df_excel['ID SIGS'].astype(str)
+
+            output_path = filedialog.asksaveasfilename(defaultextension=".xlsx", filetypes=[("Excel files", "*.xlsx")])
+
+            # Verificando se o arquivo de saída foi selecionado
+            if not output_path:
+                self.lbl_status.config(text="Erro: Arquivo de saída não selecionado.")
+                return
+        
+            # Salvando IDs do CSV não presentes no Excel
+            df_to_save = df_csv[~df_csv['ID'].isin(df_excel['ID SIGS'])]
+            df_to_save.to_excel(output_path, index=False, sheet_name="Cadastrar Jira")
+
+            # Salvando IDs do Excel não presentes no CSV (Verificação inversa)
+            df_to_close = df_excel[~df_excel['ID SIGS'].isin(df_csv['ID'])]
+            with pd.ExcelWriter(output_path, engine='openpyxl', mode='a') as writer:
+                df_to_close.to_excel(writer, index=False, sheet_name="Fechar Sigs")
+
+            self.lbl_status.config(text="Processo concluído!")
+
+        except Exception as e:
+            self.lbl_status.config(text=f"Erro: {str(e)}")
+            return
+
+    def on_delete_files(self):
+        self.delete_files([self.csv_path, self.xlsx_path])
+        self.lbl_status.config(text="Arquivos excluídos com sucesso!")
+
+if __name__ == "__main__":
+    root_intro = tk.Tk()
+    intro = IntroScreen(root_intro)
+    root_intro.mainloop()
